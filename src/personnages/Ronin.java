8af94f9e4bde1e89777d4346084f76def499e5dd
benantar mohamed <bnm4046a@ens.fsi>
2022-10-26 15:31:54 +0200

    fin session 2.3 en cours

---
+++ src/personnages/Ronin.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Ronin extends Humain{
+	private int honneur=1;
+
+	public Ronin(String nom, String boisson, int argent) {
+		super(nom,boisson,argent);
+	}
+	
+	public void donner(Commercant beneficiaire) {
+		don = beneficiaire.getArgent
+	}
+}
